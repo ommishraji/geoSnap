32f549c236f345a8e86c57441d1f05390f19eea7
ommishra.ji <ommishrapk@gmail.com>
2024-08-22 22:43:58 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:untitled1/screens/Home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async{
-  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
